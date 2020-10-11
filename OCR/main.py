9fbd4c022d48b4318baabdd26187346f4a631e67
jji8 <jj@i8labs.io>
2020-10-11 12:12:56 +0530

    Commit

--- OCR/main.py
+++ OCR/main.py
@@ -1,21 +1,32 @@
+import os
 from PIL import Image 
 import pytesseract
+
+arr = os.listdir("./data")
+try:
+    arr.remove(".DS_Store")
+except:
+    print("No .DS_Store File found") 
+
 outfile = "out_text.txt"
 
 f = open(outfile, "a") 
   
-filename = "sample.png"
 
-# Recognize the text as string in image using pytesserct 
-text = str(((pytesseract.image_to_string(Image.open(filename))))) 
+for file in arr: 
+    filename = file
+    print("processing file : " + filename )
+    f = open(outfile, "a") 
+    # Recognize the text as string in image using pytesserct 
+    text = str(((pytesseract.image_to_string(Image.open("./data/"+filename))))) 
 
-# In many PDFs, at line ending, if a word can't 
-# be written fully, a 'hyphen' is added. 
-# To remove this, we replace every '-\n' to ''. 
-text = text.replace('-\n', '')     
+    # In many PDFs, at line ending, if a word can't 
+    # be written fully, a 'hyphen' is added. 
+    # To remove this, we replace every '-\n' to ''. 
+    text = text.replace('-\n', '')     
 
-# Finally, write the processed text to the file. 
-f.write(text) 
+    # Finally, write the processed text to the file. 
+    f.write(text) 
   
-# Close the file after writing all the text. 
-f.close() 
+    # Close the file after writing all the text. 
+    f.close()
